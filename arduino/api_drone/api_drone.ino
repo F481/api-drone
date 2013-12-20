495acc1217be02065c7a6adac110486c4f7255e7
Fabian Frick <fabi.kcirf@gmail.com>
2013-12-20 23:04:14 +0100

    we now can call function via serial (needed for rasp pi communication

--- arduino/api_drone/api_drone.ino
+++ arduino/api_drone/api_drone.ino
@@ -1,18 +1,20 @@
 #include <Servo.h>
   
-#define MOTOR1_PIN  9;
-#define MOTOR2_PIN 10;
+#define MOTOR1_PIN  9
+#define MOTOR2_PIN 10
 
 // we use the servo library to control the motors
 Servo motor1, motor2, motor3, motor4;
 
 // mpx permax 400 brushed motors needs at least 80 to start (7,4V Lipo)
 int min_speed = 80;
+
+String input = "";
   
 void setup()
 {
   Serial.begin(9600);
-  Serial.print("Ready\n");
+  Serial.print("Initializing... ");
 
   // calibrate motors during setup, otherwise they wouldn't start
   motor1.attach(MOTOR1_PIN);
@@ -24,26 +26,90 @@ void setup()
   
   // wait a second to synchronize the motors
   delay(1000);
+  Serial.println("Ready!");
 }
 
 void loop()
 {
-  //startMotors();
-  //delay(5000);
-  //stopMotors();
-  //delay(5000);
+  char character;
+  
+  // read commands from serial
+  while(Serial.available()) {
+    character = Serial.read();
+    input.concat(character);
+    delay(2);
+  }
+  
+  if (input != "") {
+    String result = handleCommand(input);
+    Serial.println(result);
+    input = "";
+  }
 }
+
+// format: <command>[:argument] (where ":argument" is optional)
+String handleCommand(String input) {
+  String result;
+  
+  String command = getCommandFromInput(input);
+  int argument = getArgumentFromInput(input);
+
+  // switch-case-construct would be better, but it's not supporting strings as case value
+  if (command == "start") {
+    result = startMotors();
+  } else if (command == "stop") {
+    result = stopMotors();
+  } else {
+    result = "unknown command: " + command;
+  }
+    
+  return result;
+}
+
+
+// parses the command from serial input
+String getCommandFromInput(String input) {
+  String result = input;
+  int positionOfColon = input.indexOf(':');
+  
+  if (positionOfColon) {
+    result = input.substring(0, positionOfColon);
+  }
+ 
+  return result;
+}
+
+
+// parses the argument (is any given) from serial input
+int getArgumentFromInput(String input) {
+  int result = -1;
+  int positionOfColon = input.indexOf(':');
+  
+  if (positionOfColon) {
+    int length = input.length() - positionOfColon;
+    String argument = input.substring(positionOfColon + 1);
+    result = argument.toInt();
+  }
+ 
+  return result;
+}
+
 
 // before we take off let's start the motors with minimum speed
-void startMotors() {
+String startMotors() {
   motor1.write(min_speed); 
   motor2.write(min_speed);
+  
+  return "ok";
 }
 
+
 // power off the motors
-void stopMotors() {
+String stopMotors() {
   motor1.write(0);
   motor2.write(0);
+  
+  return "ok";
 }
 
 
