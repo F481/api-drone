02cb39242f5f16245c4a3b8f69647e1075cb1c02
Fabian Frick <fabi.kcirf@gmail.com>
2013-12-21 19:40:44 +0100

    auto formatted

--- arduino/api_drone/api_drone.ino
+++ arduino/api_drone/api_drone.ino
@@ -11,10 +11,12 @@ bool debugMode = true;
 
 // we use the servo library to control the motors
 Servo motor1, motor2, motor3, motor4;
-Servo motor[] = { motor1, motor2, motor3, motor4 };
+Servo motor[] = { 
+  motor1, motor2, motor3, motor4 };
 
 // maps motors to its related pins
-int motor_pin[] = { 9, 10, 11, 12 };
+int motor_pin[] = { 
+  9, 10, 11, 12 };
 
 // maybe we have to change min_speed depending on used motors, ESC and battery
 int min_speed = 80;
@@ -22,16 +24,16 @@ int max_speed = 180;
 
 int numberOfMotors = sizeof(motor) / sizeof(motor[0]);
 String input = "";
-  
+
 void setup()
 {
   // hack to make printf work..
   // http://reza.net/wordpress/?p=269
   fdevopen(&my_putc, 0);
-  
+
   Serial.begin(9600);
   Serial.println("Initializing... ");
-  
+
   // calibrate motors during setup, otherwise they wouldn't start
   for (int i = 0; i < numberOfMotors; i++) {
     motor[i].attach(motor_pin[i]);
@@ -41,7 +43,7 @@ void setup()
       printf("Motor%d successfully calibrated on pin %d \n", i+1, motor_pin[i]);
     }
   }
-  
+
   // wait a second to synchronize the motors
   delay(1000);
   Serial.println("Ready!");
@@ -50,42 +52,45 @@ void setup()
 void loop()
 {  
   char character;
-  
+
   // read commands from serial
   while(Serial.available()) {
     character = Serial.read();
     input.concat(character);
     delay(2);
   }
-  
+
   if (input != "") {
     String result = handleCommand(input);
     Serial.println(result);
     input = "";
   }
-  
+
   //Serial.println(getDistanceInCm());
 }
 
 // format: <command>[:argument] (where ":argument" is optional)
 String handleCommand(String input) {
   String result;
-  
+
   String command = getCommandFromInput(input);
   int argument = getArgumentFromInput(input);
 
   // switch-case-construct would be better, but it's not supporting strings as case value
   if (command == "start") {
     result = startMotors();
-  } else if (command == "stop") {
+  } 
+  else if (command == "stop") {
     result = stopMotors();
-  } else if (command == "speed") {
+  } 
+  else if (command == "speed") {
     //result = setMotorSpeed(1, 0);
     //result = setMotorSpeed(2, 30);
-  } else {
+  } 
+  else {
     result = "unknown command: " + command;
   }
-    
+
   return result;
 }
 
@@ -94,11 +99,11 @@ String handleCommand(String input) {
 String getCommandFromInput(String input) {
   String result = input;
   int positionOfColon = input.indexOf(':');
-  
+
   if (positionOfColon) {
     result = input.substring(0, positionOfColon);
   }
- 
+
   return result;
 }
 
@@ -107,13 +112,13 @@ String getCommandFromInput(String input) {
 int getArgumentFromInput(String input) {
   int result = -1;
   int positionOfColon = input.indexOf(':');
-  
+
   if (positionOfColon) {
     int length = input.length() - positionOfColon;
     String argument = input.substring(positionOfColon + 1);
     result = argument.toInt();
   }
- 
+
   return result;
 }
 
@@ -126,7 +131,7 @@ String startMotors() {
       printf("Starting motor%d... \n", i+1);
     }
   }
-  
+
   return "ok";
 }
 
@@ -139,7 +144,7 @@ String stopMotors() {
       printf("Stopping motor%d... \n", i+1);
     }
   }
-  
+
   return "ok";
 }
 
@@ -148,17 +153,17 @@ String setMotorSpeed(int motorNumber, int speedInPercent) {
   // map the given percentage speed to our speed range
   // for security reasons 0 percent is mapped to min_speed, if you really want to power off the motors call stopMotors()
   int motorSpeed = map(
-    speedInPercent,
-    0, 100,
-    min_speed, max_speed
-  );
-  
+  speedInPercent,
+  0, 100,
+  min_speed, max_speed
+    );
+
   // motor array start with 0
   motor[motorNumber - 1].write(motorSpeed);
   if (debugMode) {
     printf("Set speed of motor%d to %d \n", motorNumber, motorSpeed);
   }
-  
+
   return "ok";
 }
 
@@ -172,3 +177,4 @@ int getDistanceInCm() {
 int my_putc(char c, FILE *t) {
   Serial.write(c);
 }
+
